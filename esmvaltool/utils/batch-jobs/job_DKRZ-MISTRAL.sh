cf87a5b81333d6f3993e93885972366db03349d5
Evgenia Galytska <egalytska@iup.physik.uni-bremen.de>
2021-12-17 15:32:52 +0100

    Current .sh script to run recipes

--- esmvaltool/utils/batch-jobs/job_DKRZ-MISTRAL.sh
+++ esmvaltool/utils/batch-jobs/job_DKRZ-MISTRAL.sh
@@ -3,13 +3,10 @@
 ### BATCH SCRIPT TO RUN THE ESMVALTOOL AT DKRZ MISTRAL
 ### Author: Mattia Righi (DLR)
 ###############################################################################
-#SBATCH --partition=prepost
-#SBATCH --exclusive
-#SBATCH --nodes=1
-#SBATCH --mem=0
-#SBATCH --time=12:00:00
-#SBATCH --mail-type=FAIL,END
-#SBATCH --account=XXXXXX
+#SBATCH --partition=compute
+#SBATCH --time=1:00:00
+#SBATCH --mail-type=FAIL
+#SBATCH --account=bd1083
 #SBATCH --output=job_%j.out.log
 #SBATCH --error=job_%j.err.log
 ###############################################################################
@@ -17,15 +14,15 @@
 # Submit job with: sbatch job_DKRZ-MISTRAL.sh
 
 # Input arguments
-RECIPE= # e.g. recipe_perfmetrics_CMIP5.yml
-CONFIG= # e.g. config-user.yml
+RECIPE=/work/bd0854/b380971/tools4/ESMValTool/esmvaltool/recipes/galytska21/recipe_Arctic_telecon_qbo_era5_vs_ei.yml
+CONFIG=/work/bd0854/b380971/tools4/ESMValTool/config-user_EG.yml
 
 # Set environment
-CONDAPATH=  # e.g. /home/soft/miniconda3/
+CONDAPATH=/mnt/lustre01/pf/b/b380971/mambaforge
 
 # Changes below this line should not be required
 source $CONDAPATH/etc/profile.d/conda.sh
-conda activate esmvaltool
+conda activate esmvaltool_v24
 conda info --envs
 
 esmvaltool run --config-file $CONFIG $RECIPE
